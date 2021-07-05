47f82d05527dd08b054baa012d71d7a9f59beacd
Aleksey Vasilenko <aleksey.vasilenko34@gmail.com>
2021-07-05 17:24:33 +0300

    Annotations update

--- src/main/Mock/Bitrix/Main/Data/ICacheEngine.php
+++ src/main/Mock/Bitrix/Main/Data/ICacheEngine.php
@@ -5,9 +5,46 @@ namespace WebArch\BitrixTaxidermist\Mock\Bitrix\Main\Data;
 
 interface ICacheEngine
 {
+    /**
+     * @return bool
+     */
     public function isAvailable();
+
+    /**
+     * @param string $baseDir
+     * @param string|bool $initDir
+     * @param string|bool $filename
+     *
+     * @return void
+     */
     public function clean($baseDir, $initDir = false, $filename = false);
+
+    /**
+     * @param mixed $allVars
+     * @param string $baseDir
+     * @param string $initDir
+     * @param string $filename
+     * @param int $TTL
+     *
+     * @return bool
+     */
     public function read(&$allVars, $baseDir, $initDir, $filename, $TTL);
+
+    /**
+     * @param mixed $allVars
+     * @param string $baseDir
+     * @param string $initDir
+     * @param string $filename
+     * @param int $TTL
+     *
+     * @return void
+     */
     public function write($allVars, $baseDir, $initDir, $filename, $TTL);
+
+    /**
+     * @param string $path
+     *
+     * @return bool
+     */
     public function isCacheExpired($path);
 }
--- src/main/Mock/Bitrix/Main/Data/ICacheEngineStat.php
+++ src/main/Mock/Bitrix/Main/Data/ICacheEngineStat.php
@@ -5,7 +5,18 @@ namespace WebArch\BitrixTaxidermist\Mock\Bitrix\Main\Data;
 
 interface ICacheEngineStat
 {
+    /**
+     * @return integer|false
+     */
     public function getReadBytes();
+
+    /**
+     * @return integer|false
+     */
     public function getWrittenBytes();
+
+    /**
+     * @return string
+     */
     public function getCachePath();
 }
