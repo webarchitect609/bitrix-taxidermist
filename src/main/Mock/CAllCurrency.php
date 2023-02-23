8aecf3d749c1b6645bb7826bf6b5de2046e200ad
Filatov Anton <filatov@intaro.ru>
2023-02-24 00:12:56 +0300

    add CCurrency:GetBaseCurrency

--- src/main/Mock/CAllCurrency.php
+++ src/main/Mock/CAllCurrency.php
@@ -5,4 +5,13 @@ namespace WebArch\BitrixTaxidermist\Mock;
 
 class CAllCurrency
 {
+    /**
+     * @deprecated deprecated since currency 16.0.0
+     *
+     * @return string
+     */
+    public static function GetBaseCurrency()
+    {
+        return "";
+    }
 }
